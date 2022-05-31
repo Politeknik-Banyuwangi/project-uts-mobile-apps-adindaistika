fee91a5447875150ede5a0f8373a4586d9527f3c
FlutLab.io <>
2022-05-31 06:57:42 +0000

    Autosave

--- lib/Widget/CardChats.dart
+++ lib/Widget/CardChats.dart
@@ -4,52 +4,44 @@ import 'package:flutter/material.dart';
 class CardChats extends StatelessWidget {
   final String _name;
   final String _time;
-  final String _Dsc;
+  final String _dsc;
   final String _profilImage;
 
-  const CardChats(this._name, this._time, this._Dsc, this._profilImage);
+  const CardChats(this._name, this._time, this._dsc, this._profilImage);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotificationListener<OverscrollIndicatorNotification>(
-        onNotification: (_) {
-          _.disallowGlow();
-          return true;
-        },
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-
-            // Kodingan dimulai
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage('${this._profilImage}'),
-                radius: 30.0,
-              ),
-              title: Text(
-                '${this._name}',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Row(
-                children: [
-                  Icon(
-                    Icons.done_all_rounded,
-                    color: Colors.blue,
-                  ),
-                  SizedBox(
-                    width: 3.0,
-                  ),
-                  Text('${this._Dsc}'),
-                ],
-              ),
-              trailing: Text('${this._time}'),
-            ),
-          ],
+    return ListView(
+      children: [
+        SizedBox(
+          height: 10,
         ),
-      ),
+
+        // Kodingan dimulai
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage('${this._profilImage}'),
+            radius: 30.0,
+          ),
+          title: Text(
+            '${this._name}',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all_rounded,
+                color: Colors.blue,
+              ),
+              SizedBox(
+                width: 3.0,
+              ),
+              Text('${this._dsc}'),
+            ],
+          ),
+          trailing: Text('${this._time}'),
+        ),
+      ],
     );
   }
 }
